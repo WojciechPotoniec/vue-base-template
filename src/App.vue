9039313023d49967c78e1b15ce6c8bb9bf7b178c
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-10 10:58:52 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-center">Ciao</h1>
+  <i class="fa fa-solid fa-home"></i>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
